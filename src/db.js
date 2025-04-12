31ab22cc444228dcad5c328e180321127066cf78
shino172 <anhhao24062004@gmail.com>
2025-04-12 14:13:16 +0700

    fix serverjs

--- src/db.js
+++ src/db.js
@@ -1,23 +1,4 @@
-// src/db.js
-// const { Pool } = require('pg');
-// require('dotenv').config();
 
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-// });
-
-// pool.on('connect', () => {
-//   console.log('Connected to the database');
-// });
-
-// pool.on('error', (err) => {
-//   console.error('Database error:', err.stack);
-// });
-
-// module.exports = {
-//   query: (text, params) => pool.query(text, params),
-// };
 // chat-backend/db.js
 const { Pool } = require("pg");
 require("dotenv").config();
